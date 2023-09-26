b197751ec7bf1e257d753b98a5741779d81d9e9a
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-26 17:37:02 +0200

    add select with number of found card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,25 +16,33 @@ export default {
 </script>
 
 <template>
-    <main class="p-5">
+    <main class="p-3">
         <div class="container">
-            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 bg-white p-4">
-                <div class="col p-2" v-for="card in store.cards">
-                    <div class="card text-center">
-                        <img :src="card.card_images[0].image_url" alt="">
-                        <div class="body-card d-flex flex-column align-items-center justify-content-center p-3">
-                            <div class="title text-white text-uppercase">
-                                {{ card.name }}
-                            </div>
-                            <div>{{ card.archetype }}</div>
-                        </div>
-                    </div>
-                    <!-- /.card -->
+            <select class="m-3 w-25" name="type" id="type">
+                <option value="alien">Alien</option>
+            </select>
+            <div class="container px-0 bg-white">
+                <div id="cards_found" class="p-2 text-white">
+                    Found tot cards
                 </div>
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 p-4">
+                    <div class="col p-2" v-for="card in store.cards">
+                        <div class="card text-center">
+                            <img :src="card.card_images[0].image_url" alt="">
+                            <div class="body-card d-flex flex-column align-items-center justify-content-center p-3">
+                                <div class="title text-white text-uppercase">
+                                    {{ card.name }}
+                                </div>
+                                <div>{{ card.archetype }}</div>
+                            </div>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
+            <!-- /.container -->
         </div>
-        <!-- /.container -->
     </main>
 </template>
 
