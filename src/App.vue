6cffffd1412a49eafd1d60964314b285e2ab6ced
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-26 14:23:11 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+
+
+export default {
+
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+
+}
+</script>
+
+<template>
+  <h1>FUNGE!</h1>
+</template>
+
+<style scoped></style>
